596ce79657fed15951523a63bcf07c0038a74408
Radu Popovici <rpopovici@totalsoft.ro>
2020-07-02 20:15:18 +0300

    minor changes

--- packages/messaging-host/src/middleware/index.js
+++ packages/messaging-host/src/middleware/index.js
@@ -1,8 +1,5 @@
-const tenantIdentification = require("./tenantIdentification")
 const correlation = require("./correlation")
-const dbInstance = require("./dbInstance")
-const tracing = require("./tracing")
 const dispatcher = require("./dispatcher")
 const exceptionHandling = require("./exceptionHandling")
 
-module.exports = { tenantIdentification, correlation, dbInstance, dispatcher, tracing, exceptionHandling }
+module.exports = { correlation, dispatcher, exceptionHandling }
