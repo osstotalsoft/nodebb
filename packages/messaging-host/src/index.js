45cc54234d1ce2c8a8164dadf5c355c539e14550
Radu Popovici <rpopovici@totalsoft.ro>
2020-07-02 20:16:45 +0300

    minor fixes

--- packages/messaging-host/src/index.js
+++ packages/messaging-host/src/index.js
@@ -1,17 +1,11 @@
 const messagingHost = require('./messagingHost')
 const {
-  tenantIdentification,
   correlation,
-  dbInstance,
   dispatcher,
-  tracing,
   exceptionHandling,
 } = require('./middleware')
 
-messagingHost.tenantIdentification = tenantIdentification
 messagingHost.correlation = correlation
-messagingHost.dbInstance = dbInstance
-messagingHost.tracing = tracing
 messagingHost.dispatcher = dispatcher
 messagingHost.exceptionHandling = exceptionHandling
 
