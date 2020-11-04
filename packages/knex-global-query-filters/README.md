20818a925cec3fefc093b65d6cd3f8abc0ac0622
Radu Popovici <rpopovici@totalsoft.ro>
2020-11-04 07:38:51 +0200

    added package knex-global-query-filters

---
+++ packages/knex-global-query-filters/README.md
@@ -0,0 +1,9 @@
+# message-bus
+Knex global query filters
+
+## installation
+```javascript
+npm install @totalsoft/knex-global-query-filters
+```
+
+## philosophy
---
+++ packages/knex-global-query-filters/jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line node/no-unpublished-require
+const base = require("../../jest.config");
+
+module.exports = {
+    ...base,
+    name: "knex-global-query-filters",
+    displayName: "knex-global-query-filters"
+};
