b98988fc4614fd250c77beca822484c5068bf07c
drosca@totalsoft.ro <drosca@totalsoft.ro>
2022-08-25 10:26:28 +0300

    removed knex-filters

--- packages/knex-filters/src/dbSchema/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) TotalSoft.
-// This source code is licensed under the MIT license.
-
-import * as mssql from './mssql'
-import * as pg from './pg'
-
-export { mssql, pg }
--- packages/knex-filters/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) TotalSoft.
-// This source code is licensed under the MIT license.
-
-import * as dbSchema from './dbSchema'
-export { dbSchema }
-export { createFilter, registerFilter } from './filter'
